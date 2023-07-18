2501ca2c613515c437e63e27fc15eed4b575b22b
Vladislav Penev <penev.vladislav@gmail.com>
2023-07-18 15:09:17 +0300

    Replaced the compilation method with webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = [
+  {
+    entry: './src/index.ts',
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'commonjs2'
+    }
+  },
+  {
+    entry: './src/index.ts',
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    output: {
+      filename: 'index.esm.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'module'
+    },
+    experiments: {
+      outputModule: true,
+    },
+    target: ['web', 'browserslist: > 0.5%, not dead']
+  }
+];
